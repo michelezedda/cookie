14250e5b4aff492bb42c55abaed19f2f0b57ea8f
Michele <mikzed@hotmail.it>
2025-08-11 15:01:01 +0200

    GSAP implemented

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,41 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const Hero = () => {
+  useGSAP(() => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-cookie", { y: 200, x: 70 }, 0)
+      .to(".left-cookie", { y: 200, x: -70 }, 0);
+  }, []);
+
   return (
-    <header className="flex justify-center items-center w-full mt-40">
+    <header
+      className="flex justify-center items-center w-full mt-40 relative"
+      id="hero"
+    >
+      {/* Left cookie */}
+      <img
+        src="/media/chips.png"
+        alt="chocolate chips cookie"
+        className="left-cookie absolute -left-14 lg:-left-16 top-10 md:top-20 w-40 md:w-50 2xl:w-70"
+      />
+      {/* Right cookie */}
+      <img
+        src="/media/double-white.png"
+        alt="double white chocolate cookie"
+        className="right-cookie absolute  -right-14 lg:-right-10 top-1/2 md:top-150 lg:top-170 w-40 md:w-50 2xl:w-70"
+      />
       <div className="mx-auto max-w-screen-lg relative">
         <div className="flex flex-col lg:flex-row m-4">
           <h1 className="text-6xl text-white font-[Caprasimo]">
