9df61eccb1579800e3ee58789e2f707017e9f548
Michele <mikzed@hotmail.it>
2025-08-12 18:26:04 +0200

    text-size changed

--- src/components/categories/LowCalories.tsx
+++ src/components/categories/LowCalories.tsx
@@ -6,7 +6,7 @@ const LowCalories = () => {
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <h2 className="text-5xl font-[Caprasimo] mb-10">Low Calories</h2>
+      <h2 className="text-4xl font-[Caprasimo] mb-10">Low Calories</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:md:grid-cols-3 gap-y-35 gap-x-10 mt-25">
         {lowCalCookies.map((cookie) => (
           <CookieCard key={cookie.id} cookie={{ ...cookie, quantity: 1 }} />
